af56c505cbd3afd9f5279a9a87040b8f237ec890
Andy <andypurbrick@gmail.com>
2019-05-10 17:46:49 +0100

    Responsive CSS home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ScrollableAnchor from 'react-scrollable-anchor'
+import Media from 'react-media'
 
 export default class Home extends Component {
 
@@ -8,9 +9,37 @@ export default class Home extends Component {
     return (
       <ScrollableAnchor id={'home'}>
       <div className="home">
-      <p className="home_hello">Hello, I'm <span className="andy">Andy Purbrick</span>.</p>
-      <p className="home_dev">I'm a full stack web developer.</p>
-      <a className="view_work" href="#about">View my work ></a>
+
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <p className="home_hello_2">Hello, I'm <span className="andy">Andy Purbrick</span>.</p>
+          ) : (
+          <p className="home_hello">Hello, I'm <span className="andy">Andy Purbrick</span>.</p>
+        )
+      }
+      </Media>
+
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <p className="home_dev_2">I'm a full stack web developer.</p>
+        ) : (
+          <p className="home_dev">I'm a full stack web developer.</p>
+        )
+      }
+      </Media>
+
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <a className="view_work_2" href="#about">View my work ></a>
+        ) : (
+          <a className="view_work" href="#about">View my work ></a>
+        )
+      }
+      </Media>
+
       </div>
       </ScrollableAnchor>
     );
