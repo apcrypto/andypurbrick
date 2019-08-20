52462fbbf1d5062c7bcef086cf6c8096788a1ff8
Andy <andypurbrick@gmail.com>
2019-08-20 12:10:59 +0100

    re-commiting after rollback

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,77 +1,45 @@
-import React, { Component } from "react";
-import ScrollableAnchor from "react-scrollable-anchor";
-import Media from "react-media";
+import React, { Component } from "react"
+import ScrollableAnchor from "react-scrollable-anchor"
+import Media from 'react-media'
+
 
 export default class Contact extends Component {
+
+
   render() {
     return (
       <ScrollableAnchor id={"contact"}>
         <div className="contact">
-          <Media query="(max-width: 820px)">
-            {matches =>
-              matches ? (
-                <h1 className="contact_header_2">CONTACT</h1>
-              ) : (
-                <h1 className="contact_header">CONTACT</h1>
-              )
-            }
-          </Media>
+
+        <Media query="(max-width: 820px)">
+        {matches =>
+          matches ? (
+            <h1 className="contact_header_2">CONTACT</h1>
+          ) : (
+            <h1 className="contact_header">CONTACT</h1>
+          )
+        }
+        </Media>
 
           <p className="contact_underline">___</p>
 
           <Media query="(max-width: 820px)">
-            {matches =>
-              matches ? (
-                <section className="contact_links_2">
-                  <a
-                    className="contact_github_2"
-                    target="_blank"
-                    href="https://github.com/apcrypto"
-                  >
-                    Github
-                  </a>
-                  <a
-                    className="contact_linkedin_2"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/andypurbrick/"
-                  >
-                    Linkedin
-                  </a>
-                  <a
-                    className="contact_medium_2"
-                    target="_blank"
-                    href="https://medium.com/@andypurbrick"
-                  >
-                    Medium
-                  </a>
-                </section>
-              ) : (
-                <section className="contact_links">
-                  <a
-                    className="contact_github"
-                    target="_blank"
-                    href="https://github.com/apcrypto"
-                  >
-                    Github
-                  </a>
-                  <a
-                    className="contact_linkedin"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/andypurbrick/"
-                  >
-                    Linkedin
-                  </a>
-                  <a
-                    className="contact_medium"
-                    target="_blank"
-                    href="https://medium.com/@andypurbrick"
-                  >
-                    Medium
-                  </a>
-                </section>
-              )
-            }
-          </Media>
+          {matches =>
+            matches ? (
+          <section className="contact_links_2">
+          <a className="contact_github_2" target="_blank" href="https://github.com/apcrypto">Github</a>
+          <a className="contact_linkedin_2" target="_blank" href="https://www.linkedin.com/in/andypurbrick/">Linkedin</a>
+          <a className="contact_medium_2" target="_blank" href="https://medium.com/@andypurbrick">Medium</a>
+          </section>
+        ):(
+          <section className="contact_links">
+          <a className="contact_github" target="_blank" href="https://github.com/apcrypto">Github</a>
+          <a className="contact_linkedin" target="_blank" href="https://www.linkedin.com/in/andypurbrick/">Linkedin</a>
+          <a className="contact_medium" target="_blank" href="https://medium.com/@andypurbrick">Medium</a>
+          </section>
+        )
+      }
+      </Media>
         </div>
       </ScrollableAnchor>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,46 @@
 import React, { Component } from "react";
-import ScrollableAnchor from "react-scrollable-anchor";
-import Media from "react-media";
+import ScrollableAnchor from 'react-scrollable-anchor'
+import Media from 'react-media'
 
 export default class Home extends Component {
+
   render() {
+
     return (
-      <ScrollableAnchor id={"home"}>
-        <div className="home">
-          <Media query="(max-width: 820px)">
-            {matches =>
-              matches ? (
-                <p className="home_hello_2">
-                  Hello, I'm <span className="andy">Andy Purbrick</span>.
-                </p>
-              ) : (
-                <p className="home_hello">
-                  Hello, I'm <span className="andy">Andy Purbrick</span>.
-                </p>
-              )
-            }
-          </Media>
+      <ScrollableAnchor id={'home'}>
+      <div className="home">
 
-          <Media query="(max-width: 820px)">
-            {matches =>
-              matches ? (
-                <p className="home_dev_2">I'm a full stack web developer.</p>
-              ) : (
-                <p className="home_dev">I'm a full stack web developer.</p>
-              )
-            }
-          </Media>
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <p className="home_hello_2">Hello, I'm <span className="andy">Andy Purbrick</span>.</p>
+          ) : (
+          <p className="home_hello">Hello, I'm <span className="andy">Andy Purbrick</span>.</p>
+        )
+      }
+      </Media>
 
-          <Media query="(max-width: 820px)">
-            {matches =>
-              matches ? (
-                <a className="view_work_2" href="#about">
-                  View my work >
-                </a>
-              ) : (
-                <a className="view_work" href="#about">
-                  View my work >
-                </a>
-              )
-            }
-          </Media>
-        </div>
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <p className="home_dev_2">I'm a full stack web developer.</p>
+        ) : (
+          <p className="home_dev">I'm a full stack web developer.</p>
+        )
+      }
+      </Media>
+
+      <Media query="(max-width: 820px)">
+      {matches =>
+        matches ? (
+          <a className="view_work_2" href="#about">View my work ></a>
+        ) : (
+          <a className="view_work" href="#about">View my work ></a>
+        )
+      }
+      </Media>
+
+      </div>
       </ScrollableAnchor>
     );
   }
